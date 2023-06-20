06e62b264b54e84df6c8317a9ee7589e08be42fd
MyungAe <ladiescheong@naver.com>
2023-06-21 00:33:49 +0900

    feat(Signin): Assignment 3 구현, 리드미 반영

--- src/UI/page/Signin.jsx
+++ src/UI/page/Signin.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import useInput from '../../hooks/useInput';
+import { useNavigate } from 'react-router-dom';
+import api from '../../api/api';
 
 function Signin() {
-  return <div>Signin</div>;
+  const [id, IDHandler] = useInput('');
+  const [pw, PWHandler] = useInput('');
+
+  const navigate = useNavigate();
+
+  const post = async () => {
+    const response = await api.post('/auth/signin', {
+      email: id,
+      password: pw,
+    });
+
+    localStorage.setItem('access_token', response.data.access_token);
+
+    if (response.status === 200) navigate('/todo');
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    post();
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="id">아이디</label>
+        <input
+          type="email"
+          id="id"
+          value={id}
+          onChange={IDHandler}
+          data-testid="email-input"
+        />
+      </div>
+      <div>
+        <label htmlFor="pw">패스워드</label>
+        <input
+          type="password"
+          id="pw"
+          value={pw}
+          onChange={PWHandler}
+          data-testid="password-input"
+        />
+      </div>
+      <input
+        type="submit"
+        value="로그인"
+        data-testid="signin-button"
+        onClick={onSubmitHandler}
+      />
+    </form>
+  );
 }
 
 export default Signin;
