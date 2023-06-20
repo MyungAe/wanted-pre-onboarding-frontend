b7b12b4da8822e1edbccdb79b5921763ad4508c3
MyungAe <ladiescheong@naver.com>
2023-06-21 00:08:26 +0900

    feat(validations): id & pw 검증 로직 변경으로 인한 재구현

--- src/util/validations.js
+++ src/util/validations.js
@@ -1,9 +1,15 @@
-export const isCorrectID = id => {
-  if (id.indexOf('@')) return true;
+export const isCorrectAccount = (id, pw) => {
+  if (!isCorrectID(id)) return false;
+  if (!isCorrectPW(pw)) return false;
   return true;
 };
 
-export const isCorrectPW = password => {
+const isCorrectID = id => {
+  if (!id.indexOf('@')) return false;
+  return true;
+};
+
+const isCorrectPW = password => {
   if (password.length < 8) return false;
   return true;
 };
