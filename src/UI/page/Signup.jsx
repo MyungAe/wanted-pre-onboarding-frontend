9413bc48eb79bb841cbe1d5d2f3c50a63248650c
MyungAe <ladiescheong@naver.com>
2023-06-21 00:26:25 +0900

    feat(api, Signup): Assignment 2 구현 완료, 리드미 반영

--- src/UI/page/Signup.jsx
+++ src/UI/page/Signup.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import useInput from '../../hooks/useInput';
 import { isCorrectAccount } from '../../util/validations';
+import api from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
   const [id, IDHandler] = useInput('');
   const [pw, PWHandler] = useInput('');
+
+  const navigate = useNavigate();
+
+  const post = async () => {
+    const response = await api.post('/auth/signup', {
+      email: id,
+      password: pw,
+    });
+
+    if (response.status === 201) navigate('/signin');
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    post();
+  };
 
   return (
     <form>
@@ -32,6 +50,7 @@ function Signup() {
         type="submit"
         value="회원가입"
         data-testid="signup-button"
+        onClick={onSubmitHandler}
         disabled={isCorrectAccount(id, pw)}
       />
     </form>
