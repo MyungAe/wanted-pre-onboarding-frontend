746baa4cf5e9a76f2e6e228a3c5b18040b5385f2
MyungAe <ladiescheong@naver.com>
2023-06-21 00:53:26 +0900

    fix(api): 토큰 여부를 확인한 뒤 있으면 요청에 추가하도록 수정

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,10 @@ const api = axios.create({
   baseURL: 'https://www.pre-onboarding-selection-task.shop/',
 });
 
+api.interceptors.request.use(config => {
+  const access_token = localStorage.getItem('access_token');
+  if (access_token) config.headers.Authorization = `Bearer ${access_token}`;
+  return config;
+});
+
 export default api;
