350ce6c6d7803af7d134fef97726a479cc07ffec
MyungAe <ladiescheong@naver.com>
2023-06-21 00:11:05 +0900

    fix(validations): id/pw 검증 로직 수정

--- src/util/validations.js
+++ src/util/validations.js
@@ -1,15 +1,15 @@
 export const isCorrectAccount = (id, pw) => {
-  if (!isCorrectID(id)) return false;
-  if (!isCorrectPW(pw)) return false;
-  return true;
+  if (!isCorrectID(id)) return true;
+  if (!isCorrectPW(pw)) return true;
+  return false;
 };
 
 const isCorrectID = id => {
-  if (!id.indexOf('@')) return false;
-  return true;
+  if (id.indexOf('@')) return true;
+  return false;
 };
 
 const isCorrectPW = password => {
-  if (password.length < 8) return false;
-  return true;
+  if (password.length > 7) return true;
+  return false;
 };
