1fedbb86a887e4f2a6b8862c2a8ea9baeacea28b
MyungAe <ladiescheong@naver.com>
2023-06-21 00:54:37 +0900

    feat(Todo): Assignment 5 구현, 리드미 반영

--- src/UI/page/Todo.jsx
+++ src/UI/page/Todo.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import api from '../../api/api';
+import useInput from '../../hooks/useInput';
 
 function Todo() {
   const navigate = useNavigate();
@@ -8,7 +10,60 @@ function Todo() {
     if (!localStorage.getItem('access_token')) navigate('/signin');
   }, []);
 
-  return <div>Todo</div>;
+  const [newTodo, newTodoHandler] = useInput('');
+
+  const createTodo = async () => {
+    const response = await api.post('/todos', { todo: newTodo });
+    console.log(response);
+  };
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    createTodo();
+  };
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
+  const getTodo = async () => {
+    const response = await api.get('/todos');
+    setTodos(response.data);
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          id="todoInput"
+          onChange={newTodoHandler}
+        />
+        <input
+          type="submit"
+          value="추가"
+          onClick={onSubmitHandler}
+        />
+      </form>
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.ischecked}
+                />
+                <span>{todo.todo}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default Todo;
