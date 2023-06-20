995a6a6db393b96b2318f391844e635bc12c3a47
MyungAe <ladiescheong@naver.com>
2023-06-21 00:11:49 +0900

    feat(Signup): Assignment 1 구현 완료, 리드미 반영

--- src/UI/page/Signup.jsx
+++ src/UI/page/Signup.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import useInput from '../../hooks/useInput';
+import { isCorrectAccount } from '../../util/validations';
 
 function Signup() {
-  return <div>Signup</div>;
+  const [id, IDHandler] = useInput('');
+  const [pw, PWHandler] = useInput('');
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="id">아이디</label>
+        <input
+          type="email"
+          id="id"
+          value={id}
+          onChange={IDHandler}
+          data-testid="email-input"
+        />
+      </div>
+      <div>
+        <label htmlFor="pw">패스워드</label>
+        <input
+          type="password"
+          id="pw"
+          value={pw}
+          onChange={PWHandler}
+          data-testid="password-input"
+        />
+      </div>
+      <input
+        type="submit"
+        value="회원가입"
+        data-testid="signup-button"
+        disabled={isCorrectAccount(id, pw)}
+      />
+    </form>
+  );
 }
 
 export default Signup;
