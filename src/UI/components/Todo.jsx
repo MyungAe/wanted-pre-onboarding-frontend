8949bf3dbbb57b9ad9794b8421bc2e7ab9f2bae9
MyungAe <ladiescheong@naver.com>
2023-06-21 03:18:03 +0900

    feat(Todo): 수정 및 삭제 성공 시 UI에 바로 반영되도록 구현

--- src/UI/components/Todo.jsx
+++ src/UI/components/Todo.jsx
@@ -4,84 +4,91 @@ import useInput from '../../hooks/useInput';
 import { isSuccess } from '../../util/validations';
 
 function Todo({ id, isCompleted, todo }) {
+  const [currentTodo, setCurrentTodo] = useState(todo);
   const [isEditTodo, setIsEditTodo] = useState(false);
-
-  const getTodo = async () => {
-    await api.get('/todos');
-  };
+  const [isFinish, setIsFinish] = useState(isCompleted);
+  const [isDelete, setIsDelete] = useState(false);
 
   const updateTodo = async (id, todo, isCompleted) => {
     const response = await api.put(`/todos/${id}`, {
       todo,
       isCompleted: !isCompleted,
     });
-    if (isSuccess(response.status)) getTodo();
+    if (isSuccess(response.status)) setIsFinish(!isFinish);
   };
 
   const deleteTodo = async id => {
     const response = await api.delete(`/todos/${id}`);
-    if (isSuccess(response.status)) getTodo();
+    console.log(response, isSuccess(response.status));
+    if (isSuccess(response.status)) setIsDelete(true);
   };
 
   const [modifyTodo, modifyTodoHandler] = useInput(todo);
 
   return (
     <>
-      <li>
-        <label>
-          <input
-            type="checkbox"
-            defaultChecked={isCompleted}
-            onClick={() => updateTodo(id, todo, isCompleted)}
-          />
-          {isEditTodo ? (
-            <>
-              <input
-                data-testid="modify-input"
-                value={modifyTodo}
-                onChange={modifyTodoHandler}
-              />
-              <button
-                data-testid="submit-button"
-                onClick={() => {
-                  api
-                    .put(`/todos/${id}`, {
-                      todo: modifyTodo,
-                      isCompleted,
-                    })
-                    .then(response => {
-                      if (isSuccess(response.status)) setIsEditTodo(false);
-                    });
-                }}
-              >
-                제출
-              </button>
-              <button
-                data-testid="cancel-button"
-                onClick={() => setIsEditTodo(false)}
-              >
-                취소
-              </button>
-            </>
-          ) : (
-            <>
-              <span>{todo}</span>
-              <button
-                data-testid="modify-button"
-                onClick={() => setIsEditTodo(true)}
-              >
-                수정
-              </button>
-              <button
-                data-testid="delete-button"
-                onClick={() => deleteTodo(id)}
-              >
-                삭제
-              </button>
-            </>
-          )}
-        </label>
-      </li>
+      {isDelete ? (
+        <></>
+      ) : (
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              defaultChecked={isFinish}
+              onClick={() => updateTodo(id, todo, isCompleted)}
+            />
+            {isEditTodo ? (
+              <>
+                <input
+                  data-testid="modify-input"
+                  value={modifyTodo}
+                  onChange={modifyTodoHandler}
+                />
+                <button
+                  data-testid="submit-button"
+                  onClick={() => {
+                    api
+                      .put(`/todos/${id}`, {
+                        todo: modifyTodo,
+                        isCompleted,
+                      })
+                      .then(response => {
+                        if (isSuccess(response.status)) {
+                          setCurrentTodo(modifyTodo);
+                          setIsEditTodo(false);
+                        }
+                      });
+                  }}
+                >
+                  제출
+                </button>
+                <button
+                  data-testid="cancel-button"
+                  onClick={() => setIsEditTodo(false)}
+                >
+                  취소
+                </button>
+              </>
+            ) : (
+              <>
+                <span>{currentTodo}</span>
+                <button
+                  data-testid="modify-button"
+                  onClick={() => setIsEditTodo(true)}
+                >
+                  수정
+                </button>
+                <button
+                  data-testid="delete-button"
+                  onClick={() => deleteTodo(id)}
+                >
+                  삭제
+                </button>
+              </>
+            )}
+          </label>
+        </li>
+      )}
     </>
   );
 }
